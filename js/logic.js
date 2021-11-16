2ee687e20a27090e40dd7f916fea9e095fdb994c
lmodenbach <lmodenbach@gmail.com>
2021-11-16 07:46:51 -0800

    redirects are working!

--- js/logic.js
+++ js/logic.js
@@ -1,9 +1,17 @@
-//create dataset of collage titles
+//create dataset and read collage titles
 d3.csv("Business/Collages.csv").then((importedData) => {
     var data = importedData; 
     var collage_titles =  data.map((row) => row.TITLE);
 
+//populate page title using d3
+    d3.select("title")
+    .text("Home");
+
 //populate dropdown menu using d3
+    d3.select("#dropdown")
+    .append("option")
+    .text("Home");    
+
     d3.select("#dropdown")
     .selectAll("option")
     .data(collage_titles)
@@ -13,7 +21,28 @@ d3.csv("Business/Collages.csv").then((importedData) => {
         return title;
     });
 
-    d3.select("#dropdown")
-    .append("option")
-    .text("Home");
+//set up listener for drop down menu
+    var dropdownMenu = d3.selectAll("#dropdown");
+    dropdownMenu.on("change", filterPage);
+ 
+//define dropdown function
+    function filterPage() {
+
+        if (d3.event != null) {
+            d3.event.preventDefault();
+          }
+
+//redirect page
+        userSelect = d3.select("#dropdown option:checked").text(); 
+
+        var linkTitle = userSelect.split(" ").join("");
+        window.location.href = "../" + linkTitle + ".html";         
+        
+//populate page title using d3
+        d3.select("title")
+        .text(userSelect);
+
+    }
+
+
 });
