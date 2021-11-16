b5374f8f2bc18d95fe26f87dee33d042b4d25a6b
lmodenbach <lmodenbach@gmail.com>
2021-11-16 12:15:43 -0800

    progress
    have 2 js now

---
+++ js/auxLogic.js
@@ -0,0 +1,71 @@
+//create dataset and read collage titles
+d3.csv("Business/Collages.csv").then((importedData) => {
+    var data = importedData; 
+    var collage_titles =  data.map((row) => row.TITLE);
+
+//populate dropdown menu using d3
+    collage_titles.unshift("Home");
+    
+    d3.select("#dropdown")
+    .selectAll("option")
+    .data(collage_titles)
+    .enter()
+    .append("option")
+    .text(function (title) { 
+        return title;
+    });
+
+//set up listener for drop down menu
+    var dropdownMenu = d3.selectAll("#dropdown");
+    dropdownMenu.on("change", filterPage);
+ 
+//get collage title
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+    console.log(page);    
+
+    var collageTitle = "";
+//populate page title using d3
+    d3.select("title")
+    .text(collageTitle);
+
+//define dropdown function
+    function filterPage() {
+
+        if (d3.event != null) {
+            d3.event.preventDefault();
+          }
+
+//redirect page
+        var userSelect = d3.select("#dropdown option:checked").text(); 
+
+        if (userSelect === "Home")
+            window.location.href = "../index.html";
+
+        else {
+            var linkTitle = userSelect.split(" ").join("");
+            window.location.href = "../" + linkTitle + ".html"; 
+            // if (d3.event != null) {
+            //     d3.event.preventDefault();
+            // }
+
+//show selection on select
+            //   d3.select("#dropdown")
+            //   .selectAll("option")
+            //   .data(collage_titles)
+            //   .enter()
+            //   .append("option")
+            //   .text(function (title) { 
+            //         return title;
+            //   });
+
+//populate page title using d3
+            // console.log(userSelect);
+            // d3.select("title")
+            // .text(userSelect);  
+            
+//
+
+        }   
+    }
+});
